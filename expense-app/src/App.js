0f604ca7c74f6c9b121407de8cebbd6875db37e9
Can Ivit <canivit@users.noreply.github.com>
2022-07-16 23:38:17 -0400

    move initial expense data to seperate file

--- expense-app/src/App.js
+++ expense-app/src/App.js
@@ -2,53 +2,10 @@ import { useState } from "react";
 import { Container } from "react-bootstrap";
 import Expenses from "./components/Expenses";
 import NewExpense from "./components/NewExpense";
-
-const INITIAL_EXPENSES = [
-  {
-    id: 1,
-    title: "New Macbook Pro M1 Pro 16in Laptop",
-    amount: 3015.43,
-    date: new Date(2022, 7, 21),
-  },
-
-  {
-    id: 2,
-    title: "Eversource Electric Bill",
-    amount: 42.97,
-    date: new Date(2022, 6, 15),
-  },
-
-  {
-    id: 3,
-    title: "Dell Precision 5560",
-    amount: 2675,
-    date: new Date(2021, 9, 16),
-  },
-
-  {
-    id: 4,
-    title: "Apartment Rent",
-    amount: 2200,
-    date: new Date(2021, 4, 9),
-  },
-
-  {
-    id: 5,
-    title: "AllSaints Leather Jacket",
-    amount: 1368.72,
-    date: new Date(2021, 2, 13),
-  },
-
-  {
-    id: 6,
-    title: "Verizon Pre Paid Plan Renewal",
-    amount: 35,
-    date: new Date(2020, 5, 19),
-  },
-];
+import initialExpenses from "./expenses";
 
 const App = () => {
-  const [expenses, setExpenses] = useState(INITIAL_EXPENSES);
+  const [expenses, setExpenses] = useState(initialExpenses);
 
   const addExpenseHandler = (newExpenseData) => {
     setExpenses((prevExpenses) => [
---
+++ expense-app/src/expenses.js
@@ -0,0 +1,45 @@
+const expenses = [
+  {
+    id: 1,
+    title: "New Macbook Pro M1 Pro 16in Laptop",
+    amount: 3015.43,
+    date: new Date(2022, 7, 21),
+  },
+
+  {
+    id: 2,
+    title: "Eversource Electric Bill",
+    amount: 42.97,
+    date: new Date(2022, 6, 15),
+  },
+
+  {
+    id: 3,
+    title: "Dell Precision 5560",
+    amount: 2675,
+    date: new Date(2021, 9, 16),
+  },
+
+  {
+    id: 4,
+    title: "Apartment Rent",
+    amount: 2200,
+    date: new Date(2021, 4, 9),
+  },
+
+  {
+    id: 5,
+    title: "AllSaints Leather Jacket",
+    amount: 1368.72,
+    date: new Date(2021, 2, 13),
+  },
+
+  {
+    id: 6,
+    title: "Verizon Pre Paid Plan Renewal",
+    amount: 35,
+    date: new Date(2020, 5, 19),
+  },
+];
+
+export default expenses;
